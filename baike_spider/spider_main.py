854c01fc63f30ab8e434e1d377604773e46ecb05
mayJJ <18174083007@163.com>
2017-03-16 19:09:45 +0800

    Update spider_main.py

--- baike_spider/spider_main.py
+++ baike_spider/spider_main.py
@@ -17,10 +17,7 @@ class SpiderMain:
     def craw(self, root_url):
         count = 1
         self.urls.add_new_url(root_url)
-#         while self.urls.has_new_url():
-#             try:
-                #注意变量名，urls，不是url,还是太粗心
-                #new_url = self.url.get_new_url()
+
                 
         for x in range(100):
             new_url = self.urls.get_new_url() # 添加单个url
@@ -28,21 +25,17 @@ class SpiderMain:
             html_cont = self.downloader.download(new_url)
 
  
-#                  new_urls, new_data = self.parser.parse(new_url, html_cont)
+
             new_urls,new_data = self.parser.parser(new_url, html_cont)
             self.urls.add_new_urls(new_urls)  # 添加批量url
             self.outputer.collect_data(new_data)
                 
-                
-                
-#                 if count == 10:
-#                     break
-#              
+
+              
             count = count+1
-#             except:
-#                 print("craw failed")
+
         
-#         .output_html()
+
         self.outputer.output_data()
              
 
